be611354073849ca486868a60e2274b2555f3178
aierus <clarkui@bc.edu>
2023-03-21 11:51:47 -0400

    update model

--- Delivery3/TAMonitor/summary/models.py
+++ Delivery3/TAMonitor/summary/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 
-# Create your models here.
 class Course(models.Model):
-    CourseID   = models.CharField(max_length=255)
-    Name       = models.CharField(max_length=255)
-    Instructor = models.CharField(max_length=255)
-    SeatData   = models.CharField(max_length=255)
-    Rooms      = models.CharField(max_length=255)
-    Times      = models.CharField(max_length=255)
+    CourseID    = models.CharField(max_length=255)
+    Name        = models.CharField(max_length=255)
+    Instructor  = models.CharField(max_length=255)
+    Description = models.CharField(max_length=2056, null=True)
+    SeatData    = models.CharField(max_length=255)
+    Rooms       = models.CharField(max_length=255)
+    Times       = models.CharField(max_length=255)
+    NumberOfTAs = models.IntegerField(null=True)
+    WithDiscussion = models.IntegerField(null=True)
+    GradedInMeeting = models.IntegerField(null=True)
+    OfficeHours = models.IntegerField(null=True)
+    ExtraInfo   = models.CharField(max_length=2056, null=True)
+
+    def __str__(self):
+      return self.Name
+
