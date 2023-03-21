a6f223abb5455b644d588f5e574ebbb5ced42861
You Peng <52520465+tp2716@users.noreply.github.com>
2023-03-21 15:14:04 -0400

    Add files via upload

---
+++ Delivery3/TAMonitor/admins/admin.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.contrib import admin
+from django.contrib.auth.models import Group
+from summary.models import Course
+
+class CourseAdminForm(forms.ModelForm):
+    
+    class Meta:
+        model = Course
+        fields = ('Name', 'CourseID', 'Instructor', 'SeatData', 'Rooms', 'Times')
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('Name', 'CourseID', 'Instructor', 'SeatData', 'Rooms', 'Times', 'TAs')
+    list_filter = ('Name', 'CourseID', 'Instructor', 'SeatData', 'Rooms', 'Times', 'TAs')
+    form = CourseAdminForm
+
+admin.site.register(Course, CourseAdmin)
+admin.site.unregister(Group)
+admin.site.site_header = "Boston College"
