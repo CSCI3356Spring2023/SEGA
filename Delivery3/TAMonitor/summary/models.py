dd52d14e9eefcb65126e1bbf21578e5a21d9f4db
You Peng <52520465+tp2716@users.noreply.github.com>
2023-03-21 00:02:49 -0400

    Add files via upload

--- Delivery3/TAMonitor/summary/models.py
+++ Delivery3/TAMonitor/summary/models.py
@@ -2,9 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Course(models.Model):
+
     CourseID   = models.CharField(max_length=255)
     Name       = models.CharField(max_length=255)
     Instructor = models.CharField(max_length=255)
     SeatData   = models.CharField(max_length=255)
     Rooms      = models.CharField(max_length=255)
     Times      = models.CharField(max_length=255)
+    TAs        = models.CharField(null=True, default='', max_length=8)
+    
+    def __str__(self):
+        return self.Name
