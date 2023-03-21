f26e89fe0d6ad3ff6971986112d56362dd2b2387
aierus <clarkui@bc.edu>
2023-03-21 14:39:35 -0400

    update model

--- Delivery3/TAMonitor/summary/models.py
+++ Delivery3/TAMonitor/summary/models.py
@@ -16,3 +16,64 @@ class Course(models.Model):
 
     def __str__(self):
       return self.Name
+
+class Account(models.Model):
+    PERMISSIONS = [
+        ('STUDENT', 'Student'),
+        ('INSTRUCTOR', 'Instructor'),
+        ('ADMIN', 'Admin'),
+    ]
+
+    firstname = models.CharField(max_length=64)
+    lastname = models.CharField(max_length=64)
+    bcemail = models.CharField(max_length=64, unique=True)
+    password = models.CharField(max_length=64)
+    permissions = models.CharField(max_length=16, choices=PERMISSIONS, default='Student')
+
+    def __str__(self):
+      return self.bcemail
+
+class Student(Account):
+    YEAR_IN_SCHOOL = [
+        ('FR', 'Freshman'),
+        ('SO', 'Sophomore'),
+        ('JR', 'Junior'),
+        ('SR', 'Senior'),
+        ('GR', 'Graduate'),
+    ]
+
+    year_in_school = models.CharField(
+        max_length=2,
+        choices=YEAR_IN_SCHOOL,
+        default='Freshman',
+    )
+
+    OPEN_TO_WORK = [
+        ('YES', 'Open to work'),
+        ('NO', 'Not open to work'),
+    ]
+
+    major = models.CharField(max_length=64)
+    eagleid = models.CharField(max_length=16)
+    work = models.CharField(max_length=16, choices=OPEN_TO_WORK, default='NO')
+
+    def __str__(self):
+      return self.bcemail
+
+class Instructor(Account):
+    position = models.CharField(max_length=255) # e.g CS Professor
+
+    def __str__(self):
+      return self.bcemail
+
+class Admin(Account):
+    # inherited from Account
+    # firstname = models.CharField(max_length=64)
+    # lastname = models.CharField(max_length=64)
+    # bcemail = models.CharField(max_length=64)
+    # password = models.CharField(max_length=64)
+
+    positions = models.CharField(max_length=255) # e.g IT Admin
+
+    def __str__(self):
+      return self.bcemail
