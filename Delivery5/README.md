d8a382d9f4b54704c3c4fa6ae797453137598995
You Peng <52520465+tp2716@users.noreply.github.com>
2023-04-19 17:45:30 -0400

    Add files via upload

--- Delivery5/README.md
+++ Delivery5/README.md
@@ -1,1 +1,11 @@
-incorporated admin-fix
+UNIX/MAC OS:
+Run `source sourcevenv.sh` script to initialize the python virtual environment
+
+Windows:
+1. At ./Delivery4, Run `py -m venv myworld`
+2. Run `sourcevenv.bat` script to initialize the python virtual environment
+
+Run `deactivate` to exit the python venv
+
+Upon first time installing Django in  venv, do `py manage.py makemigrations` and  `py manage.py migrate`\
+To run the server, navigate to the TAMonitor folder and run `py manage.py runserver`
---
+++ Delivery5/TAMonitor/TAMonitor/forms.py
@@ -0,0 +1,104 @@
+from django.forms import ModelForm, TextInput, PasswordInput, Select
+from django import forms
+from summary.models import Account, Student, Instructor, Admin
+
+
+class RegisterForm(forms.ModelForm):
+    class Meta:
+        model = Account
+        fields = ["firstname", "lastname", "bcemail", "password", "permissions"]
+        widgets = {
+            'firstname': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;',
+                'placeholder': 'First Name'
+                }),
+            'lastname': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;',
+                'placeholder': 'Last Name'
+                }),
+            'bcemail': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;',
+                'placeholder': 'BC Email'
+                }),
+            'password': PasswordInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;',
+                'placeholder': 'Password'
+                }),
+            'permissions': Select(),
+        }
+
+class StudentRegisterForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = ["firstname", "lastname", "bcemail", "password", "year_in_school", "major", "eagleid", "work"]
+        widgets = {
+            'firstname': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;',
+                'placeholder': 'First Name'
+                }),
+            'lastname': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;',
+                'placeholder': 'Last Name'
+                }),
+            'bcemail': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;',
+                'placeholder': 'BC Email'
+                }),
+            'password': PasswordInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;',
+                'placeholder': 'Password'
+                }),
+            'year_in_school': Select(),
+            'major': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;',
+                'placeholder': 'Major'
+                }),
+            'eagleid': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;',
+                'placeholder': 'Eagle ID'
+                }),
+            'work': Select(),
+        }
+
+class InstructorRegisterForm(forms.ModelForm):
+    class Meta:
+        model = Instructor
+        fields = ["firstname", "lastname", "bcemail", "password", "position"]
+        widgets = {
+            'firstname': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;',
+                'placeholder': 'First Name'
+                }),
+            'lastname': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;',
+                'placeholder': 'Last Name'
+                }),
+            'bcemail': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;',
+                'placeholder': 'BC Email'
+                }),
+            'password': PasswordInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;',
+                'placeholder': 'Password'
+                }),
+             'position': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;',
+                'placeholder': 'Positions e.g CS Professor'
+            }),
+        }
+
