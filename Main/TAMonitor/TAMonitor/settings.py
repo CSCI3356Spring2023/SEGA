592b465a09a3adec3e2b1fc4904ec81062ce192d
You Peng <52520465+tp2716@users.noreply.github.com>
2023-05-10 18:46:07 -0400

    Update settings.py

--- Main/TAMonitor/TAMonitor/settings.py
+++ Main/TAMonitor/TAMonitor/settings.py
@@ -140,7 +140,7 @@ LOGIN_URL = "/login/"
 
 AUTH_USER_MODEL = 'summary.Account'
 
-CSRF_TRUSTED_ORIGINS = ['https://tamonitor-production']
+CSRF_TRUSTED_ORIGINS = ['https://tamonitor-production', 'https://tamonitor-production.up.railway.app/']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
